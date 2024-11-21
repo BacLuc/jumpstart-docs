75bbfc416a09cdc0a4d848c0eea5fdbd95f81dc7
Lucius Bachmann <lucius.bachmann@scs.ch>
2024-11-21 13:17:23 +0100

    add-help-Passwordvalidator
    (cherry picked from commit 8b7db02a57c709a4cc2c2bc6a9ccc84dabdf9751)

--- topics/testing/code/src/main/java/ch/scs/jumpstart/passwordvalidator/exercise/PasswordValidator.java
+++ topics/testing/code/src/main/java/ch/scs/jumpstart/passwordvalidator/exercise/PasswordValidator.java
@@ -1,8 +1,47 @@
 package ch.scs.jumpstart.passwordvalidator.exercise;
 
+import java.util.Collection;
+import java.util.List;
+import org.apache.commons.lang3.StringUtils;
+
 public class PasswordValidator {
 
-  public boolean validate(String password) {
-    return false;
+  private final List<Validator> validators;
+
+  public PasswordValidator(List<Validator> validators) {
+    this.validators = validators;
+  }
+
+  public List<String> validate(String password) {
+    return validators.stream()
+        .map(val -> val.validate(password))
+        .flatMap(Collection::stream)
+        .toList();
+  }
+}
+
+interface Validator {
+  public List<String> validate(String password);
+}
+
+class Lenght implements Validator {
+
+  @Override
+  public List<String> validate(String password) {
+    if (password.length() < 12) {
+      return List.of("must have at least 12 characters.");
+    }
+    return List.of();
+  }
+}
+
+class MixedCase implements Validator {
+
+  @Override
+  public List<String> validate(String password) {
+    if (!StringUtils.isMixedCase(password)) {
+      return List.of("password must be mixed case");
+    }
+    return List.of();
   }
 }
--- topics/testing/code/src/test/java/ch/scs/jumpstart/passwordvalidator/exercise/PasswordValidatorTest.java
+++ topics/testing/code/src/test/java/ch/scs/jumpstart/passwordvalidator/exercise/PasswordValidatorTest.java
@@ -2,15 +2,100 @@ package ch.scs.jumpstart.passwordvalidator.exercise;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
+import java.util.function.Function;
+import org.apache.commons.lang3.StringUtils;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 class PasswordValidatorTest {
+
+  @Nested
+  class JavaStringTest {
+    @Test
+    void string_length() {
+      assertThat("password".length()).isEqualTo(8);
+    }
+
+    @Test
+    void mixed_case() {
+      assertThat(StringUtils.isMixedCase("aA")).isTrue();
+    }
+
+    @Test
+    void is_numeric() {
+      assertThat(StringUtils.isNumeric("1")).isTrue();
+      assertThat(StringUtils.isNumeric("1A")).isFalse();
+    }
+
+    @Test
+    void contains_special_char() {
+      Function<String, Boolean> containsSpecialChar =
+          (input) -> {
+            for (String s : input.split("")) {
+              if (!Character.isLetterOrDigit(s.charAt(0))) {
+                return true;
+              }
+            }
+            return false;
+          };
+      assertThat(containsSpecialChar.apply("+")).isTrue();
+    }
+
+    interface Validator {
+      List<String> validate(String password);
+    }
+
+    class LengthValidator implements Validator {
+
+      @Override
+      public List<String> validate(String password) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'validate'");
+      }
+    }
+    ;
+
+    class NumberValidator implements Validator {
+
+      @Override
+      public List<String> validate(String password) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'validate'");
+      }
+    }
+    ;
+
+    List<Validator> validators = List.of(new LengthValidator(), new NumberValidator());
+  }
+
+  private final PasswordValidator passwordValidator =
+      new PasswordValidator(List.of(new Lenght(), new MixedCase()));
+
   @ParameterizedTest
   @CsvSource(
-      value = {"'';false"},
+      value = {
+        "'';2",
+        "'passwordpassword';1",
+        "'Passwordpassword';0",
+      },
       delimiter = ';')
-  void validates_password(String password, boolean expected) {
-    assertThat(new PasswordValidator().validate(password)).isEqualTo(expected);
+  void validates_password(String password, int numOfViolations) {
+    assertThat(passwordValidator.validate(password)).hasSize(numOfViolations);
+  }
+
+  @Nested
+  class LengthTest {
+    @Test
+    void empty_when_valid() {
+      assertThat(new Lenght().validate("passwordpassword")).isEqualTo(List.of());
+    }
+
+    @Test
+    void error_msg_when_violated() {
+      assertThat(new Lenght().validate("")).isEqualTo(List.of("password must be mixed case"));
+    }
   }
 }
